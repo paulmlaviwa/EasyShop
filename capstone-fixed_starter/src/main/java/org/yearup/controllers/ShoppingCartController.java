070bb16da325028ea4245c89c194645b1b80f369
clancycooper <dudeclancy@gmail.com>
2024-01-03 15:00:37 -0500

    Get Cart Feature (By User ID) Added

--- capstone-fixed_starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-fixed_starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -11,7 +15,10 @@ import org.yearup.models.User;
 import java.security.Principal;
 
 // convert this class to a REST controller
-// only logged in users should have access to these actions
+@RestController
+@CrossOrigin
+// only logged-in users should have access to these actions
+@Secured("ROLE_USER")
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -19,9 +26,14 @@ public class ShoppingCartController
     private UserDao userDao;
     private ProductDao productDao;
 
-
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping("")
     public ShoppingCart getCart(Principal principal)
     {
         try
@@ -33,7 +45,7 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            return shoppingCartDao.getByUserId(userId);
         }
         catch(Exception e)
         {
