e8e2c862e4d2f932bb1f1430d4042560805a3166
Erik Holden <erikholden4@gmail.com>
2024-01-04 08:55:26 -0500

    added cart functionality

--- capstone-fixed_starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-fixed_starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -2,6 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
@@ -16,8 +17,8 @@ import java.security.Principal;
 // convert this class to a REST controller
 @RestController
 @CrossOrigin
+@RequestMapping("cart")
 // only logged-in users should have access to these actions
-@Secured("ROLE_USER")
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -33,6 +34,7 @@ public class ShoppingCartController
 
     // each method in this controller requires a Principal object as a parameter
     @GetMapping("")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ShoppingCart getCart(Principal principal)
     {
         try
@@ -55,6 +57,7 @@ public class ShoppingCartController
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
     @PostMapping("/products/{productId}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ShoppingCartItem addCart(@PathVariable int productId, @RequestBody ShoppingCartItem shoppingCartItem, Principal principal){
         try
         {
